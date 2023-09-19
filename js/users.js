bf4be52661e0fa15160ae924db36fe05d3396ae8
Adehk <khedagermikhanova@mail.ru>
2023-09-19 16:38:39 +0300

    Добавила страницу users

---
+++ js/users.js
@@ -0,0 +1,63 @@
+const answer = fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => res.json())
+  .then((data) => renderUsers(data));
+
+const renderUsers = (data) => {
+  const cards_holder = document.querySelector(".cards-holder");
+  data.forEach((item) => {
+    cards_holder.append(createUsers(item));
+  });
+};
+
+const createUsers = (item) => {
+  const card = document.createElement("div");
+  card.classList.add("card", "w-100");
+  card.dataset.id = item.id;
+
+  const card_body = document.createElement("div");
+  card_body.classList.add("card-body");
+  card.append(card_body);
+
+  const card_name = document.createElement("h5");
+  card_name.classList.add("card-name");
+  card_body.append(card_name);
+  card_name.innerText = item.name;
+
+  const card_username = document.createElement("p");
+  card_username.classList.add("card-username");
+  card_body.append(card_username);
+  card_username.innerText = item.username;
+
+  const card_email = document.createElement("p");
+  card_email.classList.add("card-email");
+  card_body.append(card_email);
+  card_email.innerText = item.email;
+
+  const card_phone = document.createElement("p");
+  card_phone.classList.add("card-phone");
+  card_body.append(card_phone);
+  card_phone.innerText = item.phone;
+
+  const card_website = document.createElement("p");
+  card_website.classList.add("card-website");
+  card_body.append(card_website);
+  card_website.innerText = item.website;
+
+  card_body.innerHTML +=
+    '<a href="#" class="btn btn-dark">Перейти на страницу пользователя</a>';
+
+  return card;
+};
+
+{
+  /* <div class="card w-100">
+  <div class="card-body">
+    <h5 class="card-name">Leanne Graham</h5>
+    <p class="card-username">Bret</p>
+    <p class="card-email">Sincere@april.biz</p>
+    <p class="card-phone">1-770-736-8031 x56442</p>
+    <p class="card-website">hildegard.org</p>
+    <a href="#" class="btn btn-dark">Перейти на страницу пользователя</a>
+  </div>
+</div> */
+}
