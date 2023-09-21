dc958039512accbc908ef0c67ada96d924795d86
Adehk <khedagermikhanova@mail.ru>
2023-09-21 17:02:00 +0300

    Добавила небольшие поправки

--- js/script.js
+++ js/script.js
@@ -1,42 +1,44 @@
-const answer = fetch('https://jsonplaceholder.typicode.com/posts')
+const answer = fetch("https://jsonplaceholder.typicode.com/posts")
   .then((res) => res.json())
   .then((data) => renderPosts(data.splice(0, 20)));
 
 const renderPosts = (data) => {
-  const row = document.querySelector('.row');
+  const row = document.querySelector(".row");
   data.forEach((item) => {
     row.append(createPost(item));
   });
 };
 
 const createPost = (item) => {
-  const col = document.createElement('div');
-  col.classList.add('col');
+  const col = document.createElement("div");
+  col.classList.add("col");
   col.dataset.id = item.id;
   col.dataset.user_id = item.userId;
-  const card = document.createElement('div');
-  card.classList.add('card');
+  const card = document.createElement("div");
+  card.classList.add("card");
 
   col.append(card);
 
-  const card_body = document.createElement('div');
-  card_body.classList.add('card-body');
+  const card_body = document.createElement("div");
+  card_body.classList.add("card-body");
 
   card.append(card_body);
 
-  const card_title = document.createElement('h5');
-  card_title.classList.add('card-title');
-  card_title.innerText = item.title.length > 25 ? item.title.substring(0, 25) + '...' : item.title;
+  const card_title = document.createElement("h5");
+  card_title.classList.add("card-title");
+  card_title.innerText =
+    item.title.length > 25 ? item.title.substring(0, 25) + "..." : item.title;
 
   card_body.append(card_title);
 
-  const card_text = document.createElement('p');
-  card_text.classList.add('card-text');
-  card_text.innerText = item.body.length > 115 ? item.body.substring(0, 115) + '...' : item.body;
+  const card_text = document.createElement("p");
+  card_text.classList.add("card-text");
+  card_text.innerText =
+    item.body.length > 115 ? item.body.substring(0, 115) + "..." : item.body;
 
   card_body.append(card_text);
 
-  card_body.innerHTML += '<a href="#" class="btn btn-dark">Открыть пост</a>';
+  card_body.innerHTML += `<a href="post.html?postId=${item.id}" class="btn btn-dark">Открыть пост</a>`;
 
   return col;
 };
