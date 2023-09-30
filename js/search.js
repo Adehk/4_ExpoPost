789b371ac5d9ce95711e8de94461d97da5aafd49
Kheda <129729686+Adehk@users.noreply.github.com>
2023-09-30 20:38:35 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -110,7 +110,7 @@ const input = document.querySelector(".form-control", "me-2");
 
 const search = (event) => {
   event.preventDefault();
-  window.location.href = `https://adehk.github.io/4_ExpoPost/pages/search.html?name=${input.value}`;
+  window.location.href = `/4_ExpoPost/pages/search.html?name=${input.value}`;
 };
 
 searchBtn.addEventListener("click", search);
